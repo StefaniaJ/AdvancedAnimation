1c38dce053c6d90d26bcdf72f45a3014862358b2
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-11-18 16:03:29 +0100

    svg fetch start

---
+++ svg.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let courtainsSVG = document.querySelector("#Layer_1");
+
+// function start() {
+//   loadSVG();
+// }
+
+// function loadSVG() {
+//   fetch("original.svg")
+//     .then(e => e.text())
+//     .then(data => {
+//       placeSvg.innerHTML = data;
+//     });
+// }
